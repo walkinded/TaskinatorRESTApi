21af02e19e775896c04e470c0a633d571bdb197a
walkinded <labunskij1997@gmail.com>
2021-03-22 21:58:05 +0300

    Create WebSecurityConfig Add custom access in WebSecurityConfig

---
+++ src/main/java/com/taskinator/api/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.taskinator.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("USER","ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/project/all").hasRole("ADMIN")
+                .antMatchers("/api/**").hasRole("USER")
+                //.antMatchers("/api/task/**").hasRole("USER")
+                //.antMatchers("/api/project/**").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+}
